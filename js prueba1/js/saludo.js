92b375a2d4944be6faa1848fcfee3f9eb1dc3f5c
DesiTirabosque <desinoeltirabosque@gmail.com>
2024-02-08 22:03:59 -0300

    Practica JS
    Variables, estructuras repetitivas

---
+++ js prueba1/js/saludo.js
@@ -0,0 +1,76 @@
+/*var nombre= "Desiree";
+var apellido= "Tirabosque";
+var edad = 35;
+var estatura = 1.70;
+var presenteONo=true;
+
+console.log("Nombre = " + nombre + " " + typeof nombre);
+console.log("Apellido = " + apellido + " " + typeof apellido);
+console.log("Edad = " + edad + " " + typeof edad);
+console.log("Presente: " + presenteONo+ " " + typeof presenteONo);
+console.log("estatura = " + estatura + " " + typeof estatura);
+*/
+//alert("hola mundo JS " + nombre);
+
+//---------//
+
+//operaciones
+/*var totalComida=2800+1300;
+var totalBebida=900+650;
+
+var totalCena=totalBebida + totalComida;
+
+console.log("El total de la cena es: " + totalCena)*/
+
+//---------//
+
+//condicionales
+/*var  num1 = prompt("Ingrese en primer numero ");
+
+var num2= prompt("Ingrese el segundo numero");
+
+var resta = num1-num2;
+
+if(resta>0){
+    console.log("Es mayor a 0 ")
+    let imparpar=resta%2;
+    if(imparpar==0)
+    {
+        console.log("es par");
+    }else{
+        console.log("es impar");
+    }
+}else{
+    console.log("Es menor o igual a cero")
+}*/
+
+
+//---------//
+
+//while
+
+/*var contador=0;
+
+while(contador<10)
+{
+    console.log("Estoy en la vuelta N°: " + contador)
+    contador++;
+}*/
+
+//for
+
+/*
+for(var i=1;i<=10;i++)
+{
+    console.log("Vuelta N° " + i);
+}
+*/
+
+//do while
+var contador=0;
+do{
+    console.log("numero de vuelta N°: " + contador);
+    contador++;
+}while(contador<10)
+
+
